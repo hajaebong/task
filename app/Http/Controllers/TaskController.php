be6c81a7d3c592dbc9c133d6b8aba201c4c585e8
하재봉 <jb9245@betree.co.kr>
2018-10-24 17:52:51 +0900

    task컨트롤러 인증

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -2,9 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
 
 class TaskController extends Controller
 {
-    //
+    public function __construct() {
+        $this -> middleware('auth');
+    }
 }
